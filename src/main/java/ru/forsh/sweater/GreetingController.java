209b84ba60c30fcbeee221221655453c0e05dff3
Nikolay <forsh_nikolay@mail.ru>
2022-03-12 19:38:04 +0300

    fix filter issue

--- src/main/java/ru/forsh/sweater/GreetingController.java
+++ src/main/java/ru/forsh/sweater/GreetingController.java
@@ -1,8 +1,6 @@
 package ru.forsh.sweater;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,6 +39,21 @@ public class GreetingController {
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
 
         model.put("messages", messages);
 
--- src/main/java/ru/forsh/sweater/repository/MessageRepo.java
+++ src/main/java/ru/forsh/sweater/repository/MessageRepo.java
@@ -1,7 +1,13 @@
 package ru.forsh.sweater.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.forsh.sweater.domain.Message;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    @Query("select m from Message m where m.tag = ?1")
+    List<Message> findByTag(String tag);
 }
