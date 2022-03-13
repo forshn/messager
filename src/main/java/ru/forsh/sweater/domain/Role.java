a2ecaec4f28b01f37bcd596abb7d9361e5c24330
Nikolay <forsh_nikolay@mail.ru>
2022-03-13 17:44:40 +0300

    Admin access configured, user access restricted

--- src/main/java/ru/forsh/sweater/domain/Role.java
+++ src/main/java/ru/forsh/sweater/domain/Role.java
@@ -4,7 +4,7 @@ package ru.forsh.sweater.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
