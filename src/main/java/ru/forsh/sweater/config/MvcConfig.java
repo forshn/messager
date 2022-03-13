e9c2870dc0147b39275acfe268c25a8c25b915da
Nikolay <forsh_nikolay@mail.ru>
2022-03-13 20:44:17 +0300

    Add file download ability, file upload ability, static resources handlers

--- src/main/java/ru/forsh/sweater/config/MvcConfig.java
+++ src/main/java/ru/forsh/sweater/config/MvcConfig.java
@@ -1,12 +1,23 @@
 package ru.forsh.sweater.config;
 
-import org.springframework.context.annotation.Conditional;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file:///" + uploadPath + "/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/"); // означает что ресурсы будут искаться в корнях проекта, а не в общей файловой системе
+    }
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
